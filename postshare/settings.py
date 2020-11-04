988d09c406ec2f85d9697ffddd4305a81d1108df
n-pretorious <pretoriousndungu@gmail.com>
2020-11-05 02:37:05 +0300

    add static folder

--- postshare/settings.py
+++ postshare/settings.py
@@ -153,8 +153,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = "/static/"
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Redirect after login and logout
