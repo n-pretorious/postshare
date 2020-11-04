203f62a04915b16ce1d646e549946585d51faed0
n-pretorious <pretoriousndungu@gmail.com>
2020-11-05 02:05:02 +0300

    heroku config

--- postshare/settings.py
+++ postshare/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
 import cloudinary
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,4 +169,4 @@ cloudinary.config(
 )
 
 # Configure Django App for Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
