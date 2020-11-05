2a1d12e265b86af079d15bd1a32e52918edb49e4
n-pretorious <pretoriousndungu@gmail.com>
2020-11-05 11:15:30 +0300

    cors: middleware

--- postshare/settings.py
+++ postshare/settings.py
@@ -66,6 +66,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
