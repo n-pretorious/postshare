1d1bc34b6b206d1158b738270f9f43f50d09fa2a
n-pretorious <pretoriousndungu@gmail.com>
2020-11-06 01:37:28 +0300

    add post api

--- artAttack/views.py
+++ artAttack/views.py
@@ -1,15 +1,18 @@
 from artAttack.models import Art
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from artAttack.serializer import ArtSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 
 
-class ArtList(ListAPIView):
+class ArtList(ListCreateAPIView):
 
     serializer_class = ArtSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def get_queryset(self):
         return Art.objects.all()
